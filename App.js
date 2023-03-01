b2f05956c2c87fa64612d3f3cb987d8db319770b
Giteliud <githukueliud@gmail.com>
2023-03-01 16:26:05 +0300

    Revert "This is the front end code Starting backend django"
    This reverts commit 7c8b1f4f9da1f2f50e35df3680757387e5340ace.

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { HomeStack } from './navigation/stack';
-import { MyDrawer } from './navigation/drawer';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
   return (
-    <NavigationContainer>
-      {/* <HomeStack /> */}
-      <MyDrawer />
-      <StatusBar style='light' />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
